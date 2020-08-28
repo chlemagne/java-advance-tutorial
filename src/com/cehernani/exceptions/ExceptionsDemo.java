71a880308521f290fc6a44404a3fb0b68e6c72e9
Charlemagne Hernani <charlie.main@live.com.ph>
2020-08-28 14:37:51 +0800

    Try-catch MULTIPLE exceptions (1)

--- src/com/cehernani/exceptions/ExceptionsDemo.java
+++ src/com/cehernani/exceptions/ExceptionsDemo.java
@@ -1,13 +1,17 @@
 package com.cehernani.exceptions;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class ExceptionsDemo {
     public static void show() {
         try {
             var reader = new FileReader("file.txt");
+            var value = reader.read();
         } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
