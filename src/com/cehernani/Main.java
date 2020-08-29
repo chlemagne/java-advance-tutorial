c54640b23f8cd542784283b74e1c1bb2c7fa1fbe
Charlemagne Hernani <charlie.main@live.com.ph>
2020-08-29 13:08:09 +0800

    [Generics] Start Advance Tutorial

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -1,15 +1,9 @@
 package com.cehernani;
 
-import com.cehernani.exceptions.ExceptionsDemo;
-
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        try {
-            ExceptionsDemo.show3();
-        } catch(Throwable e) {
-            System.out.println("An unexpected error occurred.");
-        }
+
     }
 }
