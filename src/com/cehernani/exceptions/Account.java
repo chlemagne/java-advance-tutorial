811cf21c80ff6e55d57658ea204edfba3b388fe6
Charlemagne Hernani <charlie.main@live.com.ph>
2020-08-29 09:03:59 +0800

    Throw checked or compile-time exception

--- src/com/cehernani/exceptions/Account.java
+++ src/com/cehernani/exceptions/Account.java
@@ -1,15 +1,12 @@
 package com.cehernani.exceptions;
 
+import java.io.IOException;
+
 public class Account {
 
-    public void deposit(float value) {
+    public void deposit(float value) throws IOException{
 
-        /* data validation logic
-            - defensive programming
-            - used when building a library or framework that receives input from users or external systems
-            - otherwise, methods within an application should trust each other
-         */
         if (value <= 0)
-            throw new IllegalArgumentException(); // unchecked or runtime exception
+            throw new IOException();
     }
 }
