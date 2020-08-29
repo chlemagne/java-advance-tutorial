42c17bc2645342e9b188f7d0f2b6601e11ccfbf6
Charlemagne Hernani <charlie.main@live.com.ph>
2020-08-29 09:17:59 +0800

    Try-catch Throwable; java.lang.Object > ..Throwable > ..Exception

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -1,8 +1,15 @@
 package com.cehernani;
 
+import com.cehernani.exceptions.ExceptionsDemo;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
+        try {
+            ExceptionsDemo.show2();
+        } catch(Throwable e) {
+            System.out.println("An unexpected error occurred.");
+        }
     }
 }
