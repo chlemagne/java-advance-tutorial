fe3c8db4742c6f52526893bdc3b97efd179d9936
Charlemagne Hernani <charlie.main@live.com.ph>
2020-10-31 22:00:57 +0800

    Implement Comparable interface in Customer

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -1,11 +1,21 @@
 package com.cehernani;
 
-import com.cehernani.generics.*;
+import com.cehernani.collections.Customer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+        List<Customer> customers = new ArrayList<>();
 
+        customers.add(new Customer("b"));
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+
+        Collections.sort(customers);    // [a, b, c]
     }
 }
