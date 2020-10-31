f07a195f768e09a6f005c6ca14e34e64aeada90e
Charlemagne Hernani <charlie.main@live.com.ph>
2020-10-31 13:36:35 +0800

    Implement Iterable and Iterator interfaces in GenericList

--- src/com/cehernani/generics/GenericList.java
+++ src/com/cehernani/generics/GenericList.java
@@ -1,11 +1,14 @@
 package com.cehernani.generics;
 
+import java.util.Iterator;
+
 /*
     Java Generics syntax:
         1. Angle brackets, <>
         2. T is a standard that specifies the type of objects concerned
  */
-public class GenericList<T> {
+public class GenericList<T> implements Iterable<T> {
+
     private T[] items = (T[]) new Object[10]; // casting is done at runtime
     private int count;
 
@@ -15,5 +18,31 @@ public class GenericList<T> {
 
     public T get(int index) {
         return items[index];
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new CustomListIterator(this);
+    }
+
+    private class CustomListIterator implements Iterator<T> {
+
+        private GenericList<T> list;
+        private int index;
+
+        public CustomListIterator(GenericList<T> list) {
+            this.list = list;
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (index < list.count);
+        }
+
+        @Override
+        public T next() {
+            return list.items[index++];
+        }
     }
 }
