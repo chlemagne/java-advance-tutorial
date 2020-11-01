989f9307249ab680b7c2202bccfae6acc9819eb5
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-01 09:27:10 +0800

    (3) Refactor using Lambda Expression

--- src/com/cehernani/lambdas/LambdasDemo.java
+++ src/com/cehernani/lambdas/LambdasDemo.java
@@ -5,18 +5,47 @@ package com.cehernani.lambdas;
  *
  * 1. Functional Interface
  *      - An interface with a single abstract method (e.g. Comparable, Comparator)
+ *
+ * Variable Capture: Anonymous Inner Class vs. Lambda Expression
+ *
+ * ---------------------+-----------------------+-------------------
+ *       Aspects        | Anonymous Inner Class | Lambda Expression
+ * =====================+=======================+===================
+ * this                 | inner class           | parent class
+ * ---------------------+-----------------------+-------------------
+ * has access to parent |                       |
+ * class fields and     | no                    | yes
+ * other variables?     |                       |
+ * ---------------------+-----------------------+-------------------
+ *
  */
 public class LambdasDemo {
 
     public static void show1() {
 
-        // Anonymous Inner Class
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
+        /*
+            Lambda Expression:
+
+            Syntax:
+            1. ``->`` represents the lambda operator
+            2. String can be omitted since Java compiler would know the type of ``message`` using the signature of
+                functional interface Printer (expressed in line 38 below).
+            3. Parentheses can be removed. It is only used when:
+                a. The lambda expression has no parameters.
+                b. The lambda expression has multiple parameters.
+            4. Curly braces can be removed if the lambda expression's body only contains one line/statement.
+
+            Simplified syntax below:
+            >> greet(message -> System.out.println(message));
+
+         */
+        greet((String message) -> {
+            System.out.println(message);
         });
+
+        // other possibilities
+        Printer printer1 = new ConsolePrinter(); // concrete implementation
+        Printer printer2 = message -> System.out.println(message);
     }
 
     public static void greet(Printer printer) {
