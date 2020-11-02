a7e55fa0e76c9fda9bc49690f1cc8ed5ac537273
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-02 09:32:52 +0800

    Demonstrate UnaryOperator & BinaryOperator

--- src/com/cehernani/lambdas/DemoFunctionInterface.java
+++ src/com/cehernani/lambdas/DemoFunctionInterface.java
@@ -1,6 +1,8 @@
 package com.cehernani.lambdas;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * Built-in Functional Interfaces (java.util.function).
@@ -9,6 +11,8 @@ import java.util.function.Function;
  * 1. Consumer      void consume(obj)
  * 2. Supplier      obj supply()
  * 3. Function      obj map(obj)
+ *      -UnaryOperator
+ *      -BinaryOperator
  * 4. Predicate     bool test(condition)
  *
  */
@@ -17,6 +21,11 @@ public class DemoFunctionInterface {
     public static void show1() {
 
         Function<String, Integer> map = string -> string.length();
+        UnaryOperator<Integer> square = a -> a * a;
+        UnaryOperator<Integer> increment = a -> a + 1;
+        BinaryOperator<Integer> add = (a, b) -> a + b;
+
         int length = map.apply("Hello, world!"); // 13
+        int result = add.andThen(increment).andThen(square).apply(1, 1); // 9
     }
 }
