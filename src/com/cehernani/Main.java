071c5639f5cf5145812fbe7941cdb53689815db1
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-02 09:43:27 +0800

    [Streams] Start Advance Tutorial

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -1,17 +1,9 @@
 package com.cehernani;
 
-import com.cehernani.collections.Customer;
-import com.cehernani.collections.EmailComparator;
-import com.cehernani.lambdas.LambdasDemo;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        LambdasDemo.show1();
+
     }
 }
