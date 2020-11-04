7164d15f31598c975cb7c58310c95e4bc07f3bd2
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-04 14:20:46 +0800

    Check active thread count and available thread count

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+
+        System.out.println(Thread.activeCount());   // 2: one for main() and one for Java's garbage collector
+        System.out.println(Runtime.getRuntime()
+                .availableProcessors());            // 4: four available threads
     }
 }
