54ec5da3a01a74e94b1389b60ee00c1d8579bfc5
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-04 14:59:57 +0800

    Pause a Thread

--- src/com/cehernani/concurrency/DownloadFileTask.java
+++ src/com/cehernani/concurrency/DownloadFileTask.java
@@ -1,10 +1,25 @@
 package com.cehernani.concurrency;
 
+import java.security.SecureRandom;
+import java.util.function.Supplier;
+
 public class DownloadFileTask implements Runnable {
+
+    private SecureRandom random = new SecureRandom();
+    private Supplier<Integer> randomMilli = () -> (1 + random.nextInt(9)) * 1000;
 
     @Override
     public void run() {
-        System.out.printf("Downloading file using <%s>...%n",
+        System.out.printf("DOWNLOADING file using <%s>...%n",
+                Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(randomMilli.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("COMPLETED downloading file using <%s>!!!%n",
                 Thread.currentThread().getName());
     }
 }
