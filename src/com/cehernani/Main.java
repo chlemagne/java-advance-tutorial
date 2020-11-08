4eb0c0693bcac7d9436456c57458b9df10475603
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-08 15:40:12 +0800

    [Executive Framework] Start Advance Tutorial

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -1,11 +1,9 @@
 package com.cehernani;
 
-import com.cehernani.concurrency.*;
-
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        NonThreadSafeDemo.show2();
+
     }
 }
