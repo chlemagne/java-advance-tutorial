4bce950bc6646dea4baa09e743178dbd22d94e8c
Charlemagne Hernani <charlie.main@live.com.ph>
2020-11-08 15:29:33 +0800

    Demonstrate a non-thread safe Collections

--- src/com/cehernani/Main.java
+++ src/com/cehernani/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        AddersDemo.show1();
+        NonThreadSafeDemo.show2();
     }
 }
--- src/com/cehernani/concurrency/NonThreadSafeDemo.java
+++ src/com/cehernani/concurrency/NonThreadSafeDemo.java
@@ -4,6 +4,8 @@ import com.cehernani.concurrency.nonthreadsafe.DownloadFileTaskLocks;
 import com.cehernani.concurrency.nonthreadsafe.DownloadStatusLocks;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class NonThreadSafeDemo {
@@ -30,5 +32,36 @@ public class NonThreadSafeDemo {
 
         // print total downloaded bytes
         System.out.println(status.getTotalBytes());
+    }
+
+    public static void show2() {
+
+        Collection<Integer> collection = new ArrayList<>();
+
+        Thread thread1 = new Thread(() -> {
+            collection.addAll(Arrays.asList(1, 2, 3));
+        });
+
+        Thread thread2 = new Thread(() -> {
+            collection.addAll(Arrays.asList(4, 5, 6));
+        });
+
+        Thread thread3 = new Thread(() -> {
+            collection.addAll(Arrays.asList(7, 8, 9));
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(collection);
     }
 }
